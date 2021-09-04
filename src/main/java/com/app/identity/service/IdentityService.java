a3454ae0bb6cfd512831accc6db89b8ba72e18d1
Kobie Thomas <kobiethomas@kobies-mbp.home>
2021-09-04 14:09:33 -0400

    small change

--- src/main/java/com/app/identity/service/IdentityService.java
+++ src/main/java/com/app/identity/service/IdentityService.java
@@ -14,7 +14,6 @@ import java.util.stream.Collectors;
 
 @Service
 public class IdentityService {
-
     private final IdentityRepository identityRepository;
     private final OrgService orgService;
 
